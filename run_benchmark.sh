6959740ada6990e5fdecde5731b2c14f4cb9e21d
Ricard Lado <ricardlado@hotmail.com>
2023-01-21 23:56:03 +0100

    Automatically update rsparse

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,4 +4,5 @@ gcc -O3 -o main.o src/main.c src/csparse.c -lm && ./main.o
 
 # Run the Rust benchmark
 echo "Running benchmark for rsparse"
+cargo update # Update to the latest stable version of rsparse
 cargo run --release
